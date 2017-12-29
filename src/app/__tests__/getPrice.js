df90faa2c0f10be85622bd9383a7870f77854e35
Casey Ydenberg <casey@yourtruhome.com>
2017-12-29 15:23:39 -0700

    Write failing tests for getPrice

--- src/app/__tests__/getPrice.js
+++ src/app/__tests__/getPrice.js
@@ -1,8 +1,34 @@
+import getPrice from '../getPrice'
 
 const ajaxLibSuccess = jest.fn(() => {
-  return Promise.resolve(300000)
+  return Promise.resolve({
+    status: 200,
+    body: {price: 300000}
+  })
 })
 
 const ajaxLibFailure = jest.fn(() => {
-  return Promise.reject(new Error(500))
+  return Promise.reject(new Error({status: 500}))
+})
+
+describe('getPrice', () => {
+  it('should call the ajax library', () => {
+    getPrice('E12345', ajaxLibSuccess)
+    expect(ajaxLibSuccess).toHaveBeenCalled()
+  })
+
+  it('should return a promise that resolves to the house price', () => {
+    const obs = getPrice('E12345', ajaxLibSuccess)
+    return obs.then(result => expect(result).toEqual(300000))
+  })
+
+  it('should return a promise that resolves to the house price, using resolves', () => {
+    const obs = getPrice('E12345', ajaxLibSuccess)
+    expect(obs).resolves.toEqual(300000)
+  })
+
+  it('should return an error if the API returns an error', () => {
+    const obs = getPrice('E12345', ajaxLibFailure)
+    expect(obs).rejects.toEqual(300000)
+  })
 })
