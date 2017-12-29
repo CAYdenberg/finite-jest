28c9e8a68a6aa4d69c42774d1ef6b638e918a2be
Casey Ydenberg <casey@yourtruhome.com>
2017-12-29 09:38:37 -0700

    Write spec for formatPrice

---
+++ src/app/__tests__/formatPrice.js
@@ -0,0 +1,10 @@
+
+describe('formatPrice', () => {
+  it('should use M in place of millions of dollars')
+
+  it('should use K in place of thousands of dollars')
+
+  it('should return null for a negative number')
+
+  it('should return null for NaN')
+})
