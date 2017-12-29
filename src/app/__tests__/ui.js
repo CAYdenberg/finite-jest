bf95f0bb856cba6d2e2431d552672288bf75340b
Casey Ydenberg <casey@yourtruhome.com>
2017-12-29 16:26:15 -0700

    Add failing tests for ui

--- src/app/__tests__/ui.js
+++ src/app/__tests__/ui.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import {shallow} from 'enzyme'
+import Enzyme, {shallow} from 'enzyme'
 import UI from '../ui'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
 
 const getPrice = jest.fn()
 
-it('should render')
+it('should render', () => {
+  const comp = shallow(<UI getPrice={getPrice} />)
+  expect(comp).toBeTruthy()
+})
 
-it('should call getPrice when the button is clicked')
+it('should call getPrice when the button is clicked', () => {
+  const comp = shallow(<UI getPrice={getPrice} />)
+  comp.find('button').simulate('click')
+  expect(getPrice).toHaveBeenCalled()
+})
 
-it('should display the price')
+it('should display the price', () => {
+  const comp = shallow(<UI getPrice={getPrice} price="$300K" />)
+  expect(comp.contains('<p>$300K</p>')).toBeTruthy()
+})
--- src/app/ui.js
+++ src/app/ui.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const UI = (props) => {
+export const UI = (props) => {
   return <div />
 }
 
