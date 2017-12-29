578658ff5d9fc90c7ee989df698c8b2e062559c6
Casey Ydenberg <casey@yourtruhome.com>
2017-12-29 16:52:02 -0700

    Add full spec for getPrice

--- src/app/__tests__/getPrice.js
+++ src/app/__tests__/getPrice.js
@@ -1,8 +1,22 @@
+import getPrice from '../getPrice'
 
 const ajaxLibSuccess = jest.fn(() => {
-  return Promise.resolve(300000)
+  return Promise.resolve({
+    status: 200,
+    body: {price: 300000}
+  })
 })
 
 const ajaxLibFailure = jest.fn(() => {
-  return Promise.reject(new Error(500))
+  return Promise.resolve({
+    status: 500
+  })
+})
+
+describe('getPrice', () => {
+  it('should call the ajax library')
+
+  it('should return a promise that resolves to the house price')
+
+  it('should return an error if the API returns an error')
 })
