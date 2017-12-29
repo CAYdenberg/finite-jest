4324cbf3daa6772adad1b9f949bbd6592a6898cb
Casey Ydenberg <casey@yourtruhome.com>
2017-12-29 16:06:24 -0700

    Write spec for ui

---
+++ src/app/ui.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const UI = (props) => {
+  return <div />
+}
+
+UI.propTypes = {
+  getPrice: PropTypes.func.isRequired,
+  price: PropTypes.string
+}
+
+export default UI
