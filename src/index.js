808448aecee00a0088fa986b812b4d609a72d8bf
@CAYdenberg <ydenberg@gmail.com>
2017-12-28 20:53:05 -0700

    Get fonts working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-
+import React from 'react'
+import ReactDOM from 'react-dom'
 var Reveal = require('reveal')
 
 Reveal.initialize({
@@ -6,3 +7,37 @@ Reveal.initialize({
   progress: false,
   history: true
 })
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      housePrice: null
+    }
+
+    this._getHousePrice = this._getHousePrice.bind(this)
+  }
+
+  _getHousePrice() {
+    this.setState({
+      housePrice: 'Hello world'
+    })
+  }
+
+  render() {
+    const {state} = this
+
+    return (
+      <div>
+        <button type="button" onClick={this._getHousePrice}>Get house price</button>
+        <p>{state.housePrice}</p>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Controller />,
+  document.getElementById('app')
+)
