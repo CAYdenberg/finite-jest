52dc3abfe0a57f633aa219a2d4cd3ffc10b74ce3
@CAYdenberg <ydenberg@gmail.com>
2017-12-30 19:56:33 -0700

    Write a simple server to hand over the house price

--- src/app/getPrice.js
+++ src/app/getPrice.js
@@ -1,7 +1,7 @@
-import ajax from './ajax'
+import axios from 'popsicle'
 
-export default function(mls, ajaxLib = ajax) {
-  return ajaxLib(mls).then(res => {
+export default function(mls, makeRequest = axios.get) {
+  return makeRequest(`/house-price?mls=${mls}`).then(res => {
     if (res.status === 200) {
       return res.body.price
     } else {
